0b7c55f7ff11e94b35731598a60c7a7a18a4d32e
Bo Han <2005bhan@gmail.com>
2025-12-24 18:16:28 -0600

    third commit

---
+++ R/globals.R
@@ -0,0 +1,7 @@
+utils::globalVariables(c(
+  "pass_yards_diff_pg", "rush_yards_diff_pg", "td_diff_pg",
+  "interception_diff_pg", "fumble_diff_pg", "sack_diff_pg", "tfl_diff_pg",
+  "turnover_diff_pg", "possession_diff", "penalties_pg", "penalty_yards_pg",
+  "Ranked_WinPct", "RegSeason_WinPct", "Conf_WinPct",
+  "score", "Team", "Conference", "Rank", "Record"
+))
